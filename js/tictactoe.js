b37104b661994c97fddaf09ba64209d49c87c7cd
Dean Pooler <Deanpooler@outlook.com>
2020-11-08 22:53:25 +0100

    Add getter/setter methods

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,6 +1,14 @@
 const gameBoard = (() => {
     const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
-    
+    const getGameBoard = () => board;
+    const setGameBoard = (player, position) => {
+        if (position >= 0 || position <= 8) {
+            board[position] = player.getPlayerSymbol;
+        } else {
+            alert("Yikes. That was an invalid move.");
+        }
+    }
+    return {getGameBoard, setGameBoard};
 })();
 
 const displayController = (() => {
@@ -8,5 +16,8 @@ const displayController = (() => {
 })();
 
 const PlayerFactory = (name, symbol) => {
+    const getPlayerName = () => name;
+    const getPlayerSymbol = () => symbol;
 
+    return {getPlayerName, getPlayerSymbol};
 }
